af4d6f91f52a7d37aa2ad9d86354b0b1f5cc1fa9
Cosmin POP <mtx.rake@gmail.com>
2021-09-18 12:06:14 +0300

    error fix attempt

--- TWSelfCaster.lua
+++ TWSelfCaster.lua
@@ -1,7 +1,23 @@
 local s = CreateFrame("Frame")
+local sSetter = CreateFrame("Frame")
+sSetter:Hide()
 s:RegisterEvent("ADDON_LOADED")
 s:RegisterEvent("CVAR_UPDATE")
---s:RegisterEvent("VARIABLES_LOADED")
+s:RegisterEvent("VARIABLES_LOADED")
+
+sSetter:SetScript("OnShow", function()
+    this.startTime = GetTime()
+end)
+
+sSetter:SetScript("OnUpdate", function()
+    local plus = 1 --seconds
+    local gt = GetTime() * 1000
+    local st = (this.startTime + plus) * 1000
+    if gt >= st then
+        SetCVar("autoSelfCast", SELF_CONFIG[UnitName('player')])
+        sSetter:Hide()
+    end
+end)
 
 s:SetScript("OnEvent", function()
 
@@ -11,16 +27,14 @@ s:SetScript("OnEvent", function()
                 SELF_CONFIG = {}
                 SELF_CONFIG[UnitName('player')] = GetCVar("autoSelfCast")
             end
-            SetCVar("autoSelfCast", SELF_CONFIG[UnitName('player')])
         end
-        --if event == "VARIABLES_LOADED" then
-        --    if not SELF_CONFIG then
-        --        SELF_CONFIG = {}
-        --        SELF_CONFIG[UnitName('player')] = GetCVar("autoSelfCast")
-        --    end
-        --    if SetCVar("autoSelfCast", SELF_CONFIG[UnitName('player')]) then
-        --    end
-        --end
+        if event == "VARIABLES_LOADED" then
+            if not SELF_CONFIG then
+                SELF_CONFIG = {}
+                SELF_CONFIG[UnitName('player')] = GetCVar("autoSelfCast")
+            end
+            sSetter:Show()
+        end
         if event == "CVAR_UPDATE" then
             if arg1 == 'AUTO_SELF_CAST_TEXT' then
                 if not SELF_CONFIG then
